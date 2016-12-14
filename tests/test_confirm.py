1bd5fe60399eb9320eca54e165072fbfa33588a1
sax <s.apostolico@gmail.com>
2016-12-14 20:33:24 +0100

    removed django-dynamic-fixture dependency

--- tests/test_confirm.py
+++ tests/test_confirm.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
 from django.core.urlresolvers import reverse
-from django_dynamic_fixture import G
-import django_webtest
-import pytest
-from demo.models import DemoModel2
 
 logger = logging.getLogger(__name__)
-
-
-@pytest.fixture(scope='function')
-def app(request):
-    wtm = django_webtest.WebTestMixin()
-    wtm.csrf_checks = False
-    wtm._patch_settings()
-    request.addfinalizer(wtm._unpatch_settings)
-    return django_webtest.DjangoTestApp()
-
-
-@pytest.fixture
-def demomodel2():
-    return G(DemoModel2)
 
 
 def test_confirm(app, admin_user):
