3f8b1eae6b098186ef37d0c396015d9cdaae7882
sax <s.apostolico@gmail.com>
2021-12-01 15:48:47 +0100

    removes unused code

--- src/admin_extra_urls/checks.py
+++ src/admin_extra_urls/checks.py
@@ -10,28 +10,6 @@ def get_all_permissions():
             for p in Permission.objects.select_related('content_type').values_list('content_type__app_label', 'codename')]
 
 
-def check_decorators(cls):
-    target = cls
-    decorators = {}
-
-    def visit_FunctionDef(node):
-        deco = []
-        for n in node.decorator_list:
-            if isinstance(n, ast.Call):
-                name = n.func.attr if isinstance(n.func, ast.Attribute) else n.func.id
-            else:
-                name = n.attr if isinstance(n, ast.Attribute) else n.id
-            if name in ['href', ]:
-                deco.append(name)
-        if deco:
-            decorators[node.name] = deco
-
-    node_iter = ast.NodeVisitor()
-    node_iter.visit_FunctionDef = visit_FunctionDef
-    node_iter.visit(ast.parse(inspect.getsource(target)))
-    return decorators
-
-
 def check_decorator_errors(cls):
     target = cls
     standard_permissions = []
