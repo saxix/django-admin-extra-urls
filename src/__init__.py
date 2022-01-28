e95a7e86c72fad01abe7847ddac900d664a50e49
sax <s.apostolico@gmail.com>
2022-01-28 10:36:59 +0100

    code clean

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-NAME = 'django-admin-extra-urls'
-VERSION = __version__ = '4.0.0'
-__author__ = 'sax'
--- src/api.py
+++
@@ -1,3 +0,0 @@
-from .button import UrlButton  # noqa: F401
-from .decorators import button, href, link, url  # noqa: F401
-from .mixins import ExtraUrlMixin, confirm_action  # noqa: F401
